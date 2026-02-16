a6a8923f77d4938471afac97a5607a4952d18347
Les Aubrey <les@wharf.digital>
2026-02-16 09:42:15 +0800

    Init hive blockchain client

---
+++ packages/hive/eslint.config.ts
@@ -0,0 +1,9 @@
+import { baseConfig } from "@workspace/eslint-config/base";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig(
+  {
+    ignores: [],
+  },
+  baseConfig,
+);
